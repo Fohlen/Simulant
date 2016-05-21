1013c33dca4aa4f5edd0e2913030e4c6b3d7259c
Fohlen <lennard@berger.work>
2016-05-21 23:07:45 +0200

    Add some basic web-server (might be deprecated)

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 /**
  * Following is a web server that serves our simulations
+ * @todo Static file handling could be done using nginx - only use server.js for websocket
  */
 
+// Use yargs for easy command-line handling
+var argv = require('yargs')
+    .usage('$0 [options]')
+    .option('h', {
+        alias: 'host',
+        describe: 'The host to listen on',
+        default: 'localhost'
+    })
+    .option('p', {
+        alias: 'port',
+        describe: 'Specify the server port',
+        default: 3000
+    })
+    .help('help')
+    .argv
+    
 var express = require('express');
 var app = express();
 
 // Serve all the files from public (this is where our PaperJS application resides)
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(argv.port, argv.host, function () {
+    console.log('Simulant server running on port ' + argv.port);
 });
